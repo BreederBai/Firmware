e341cf8254ccdbb08f6e1953d59a587ee1bc8f3f
Breeder Bai <songshu_bai@icloud.com>
2018-04-26 22:02:18 +0800

    change nuttx_px4fmu-v2_default.cmake

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -1,45 +1,45 @@
 px4_nuttx_configure(HWCLASS m4 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common IO px4io-v2)
 
-#set(config_uavcan_num_ifaces 2)
+set(config_uavcan_num_ifaces 2)
 
 set(config_module_list
 	#
 	# Board support modules
 	#
 	#drivers/barometer
-	drivers/differential_pressure
+	#drivers/differential_pressure
 	#drivers/magnetometer
 	#drivers/telemetry
 
 	#drivers/imu/adis16448
-	drivers/airspeed
-	drivers/barometer/ms5611
+	#drivers/airspeed
+	#drivers/barometer/ms5611
 	#drivers/blinkm
 	#drivers/imu/bmi160
 	#drivers/barometer/bmp280
 	drivers/boards
 	#drivers/bst
-	drivers/camera_trigger
+	#drivers/camera_trigger
 	drivers/device
 	#drivers/frsky_telemetry
 	drivers/gps
 	#drivers/hott
 	#drivers/iridiumsbd
 	#drivers/irlock
-	drivers/imu/l3gd20
+	#drivers/imu/l3gd20
 	drivers/led
-	drivers/imu/lsm303d
-	drivers/magnetometer/hmc5883
-	drivers/magnetometer/lis3mdl
+	#drivers/imu/lsm303d
+	#drivers/magnetometer/hmc5883
+	#drivers/magnetometer/lis3mdl
 	#drivers/mb12xx
 	#drivers/mkblctrl
-	drivers/imu/mpu6000
-	drivers/imu/mpu9250
+	#drivers/imu/mpu6000
+	#drivers/imu/mpu9250
 	#drivers/oreoled
 	#drivers/protocol_splitter
-	drivers/pwm_input
+	#drivers/pwm_input
 	#drivers/pwm_out_sim
-	drivers/px4flow
+	#drivers/px4flow
 	drivers/px4fmu
 	drivers/px4io
 	drivers/rgbled
@@ -50,14 +50,14 @@ set(config_module_list
 	drivers/vmount
 
 	# distance sensors
-	drivers/distance_sensor/ll40ls
-	drivers/distance_sensor/mb12xx
-	drivers/distance_sensor/sf0x
-	drivers/distance_sensor/sf1xx
-	drivers/distance_sensor/srf02
-	drivers/distance_sensor/srf02_i2c
-	drivers/distance_sensor/teraranger
-	drivers/distance_sensor/tfmini
+	#drivers/distance_sensor/ll40ls
+	#drivers/distance_sensor/mb12xx
+	#drivers/distance_sensor/sf0x
+	#drivers/distance_sensor/sf1xx
+	#drivers/distance_sensor/srf02
+	#drivers/distance_sensor/srf02_i2c
+	#drivers/distance_sensor/teraranger
+	#drivers/distance_sensor/tfmini
 	#drivers/distance_sensor/ulanding
 	modules/sensors
 
@@ -69,8 +69,8 @@ set(config_module_list
 	#systemcmds/dumpfile
 	#systemcmds/esc_calib
 	systemcmds/hardfault_log
-	#systemcmds/led_control
-	systemcmds/mixer
+	systemcmds/led_control
+	#systemcmds/mixer
 	#systemcmds/motor_ramp
 	#systemcmds/motor_test
 	systemcmds/mtd
@@ -82,7 +82,7 @@ set(config_module_list
 	#systemcmds/sd_bench
 	systemcmds/top
 	#systemcmds/topic_listener
-	systemcmds/tune_control
+	#systemcmds/tune_control
 	systemcmds/ver
 
 	#
@@ -101,21 +101,21 @@ set(config_module_list
 	#
 	# General system control
 	#
-	modules/camera_feedback
-	modules/commander
-	modules/events
+	#modules/camera_feedback
+	#modules/commander
+	#modules/events
 	#modules/gpio_led
-	modules/land_detector
-	modules/load_mon
+	#modules/land_detector
+	#modules/load_mon
 	modules/mavlink
-	modules/navigator
-	#modules/uavcan
+	#modules/navigator
+	modules/uavcan
 
 	#
 	# Estimation modules
 	#
 	#modules/attitude_estimator_q
-	modules/ekf2
+	#modules/ekf2
 	#modules/local_position_estimator
 	#modules/position_estimator_inav
 	#modules/landing_target_estimator
@@ -124,13 +124,13 @@ set(config_module_list
 	#
 	# Vehicle Control
 	#
-	modules/fw_att_control
-	modules/fw_pos_control_l1
+	#modules/fw_att_control
+	#modules/fw_pos_control_l1
 	#modules/gnd_att_control
 	#modules/gnd_pos_control
-	modules/mc_att_control
-	modules/mc_pos_control
-	modules/vtol_att_control
+	#modules/mc_att_control
+	#modules/mc_pos_control
+	#modules/vtol_att_control
 
 	#
 	# Logging
